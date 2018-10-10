96e35489e7f682911373bd5b1833feb2c9be41b8
rameshpatil1 <43699508+rameshpatil1@users.noreply.github.com>
2018-10-10 18:49:23 +0530

    Update Print.py

---
+++ Print.py
@@ -0,0 +1,30 @@
+print("hello World")
+print(1+2)
+a = "HELLO"
+b = 1234
+c = 9876.123
+d = "hello world  /2342/wefa3@2334"
+# print (a.capitalize,b,c,d)
+
+print(a.lower())
+
+answer = input("what is your name ?")
+print (answer)
+
+coconuts = 2
+print("I have", coconuts, "coconuts")
+
+x = 8
+if x < 8:
+    print("x is less than eight")
+elif x == 8:
+    print("x is equal to eight")
+else:
+    print("x is greater than eight")
+
+def add(num1, num2):
+    ans = num1 + num2
+    return ans
+
+print (add(4, 6))
+
